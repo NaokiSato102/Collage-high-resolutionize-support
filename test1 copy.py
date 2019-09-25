830f93a1632d19ba241dff9389b61de7693f8c1d
NaokiSato102 <s17c1066nq@s.chibakoudai.jp>
2019-09-25 22:57:20 +0900

    いろいろ試して理解が進みつつある

--- test1 copy.py
+++ test1 copy.py
@@ -38,22 +38,12 @@ if len(good)>MIN_MATCH_COUNT:
 	matchesMask = mask.ravel().tolist()
 	
 	h,w = img1.shape[:2]
-	#h = img1.shape[0]
-	#w = img1.shape[1]
-	pts = np.float32([ [0,0],[0,h-1],[w-1,h-1],[w-1,0] ]).reshape(-1,1,2)
-	dst = cv2.perspectiveTransform(pts,M)
 
-	img2 = cv2.polylines(img2,[np.int32(dst)],True,255,3, cv2.LINE_AA)
-	cv2.imwrite('img3a.png', img2)
+	img4 = cv2.warpPerspective(img1, M, (w, h) )
+	cv2.imwrite('img4.png', img4)
 else:
 	print ('hoge')
 	matchesMask = None
 
-draw_params = dict(matchColor = (0,255,0), # draw matches in green color
-	singlePointColor = None,
-	matchesMask = matchesMask, # draw only inliers
-	flags = 2)
 
-img3 = cv2.drawMatches(img1,kp1,img2,kp2,good,None,**draw_params)
 
-cv2.imwrite('img3.png', img3)

d36623ab10746028825a44afce742f37ed8901c7
NaokiSato102 <s17c1066nq@s.chibakoudai.jp>
2019-09-29 21:46:31 +0900

    ちょっと気の迷いでファイル名を変えていたのを戻したりReadme書いたり

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # Collage-high-resolutionize-support
+学生が趣味の実益の為に作っているアプリです。いかなる損害を被っても私は責任を持ちません。
--- test1.py
+++ test1.py
@@ -1,60 +1,61 @@
+
+'''
+【opencv 基礎知識 #4】動画の手ぶれ補正をpython実装 (AKAZE, KNN, RANSAC) - MotoJapan's Tech-Memo
+http://motojapan.hateblo.jp/entry/2017/08/17/081917
+を発見することにより、ほかの記事をキーワード検索しやすくなった。
+
+画像から特徴量を抽出し、透視変換行列を導出して画像を変形する - Qiita
+https://qiita.com/ka10ryu1/items/bd05aed321a7a154d8a1
+を基に記述が抜けている部分を保管したがうまくいかなかった。
+
+試行錯誤のうちに判明したが、画像1と画像2、どちらを基準に処理しているかをしっかりと理解できずにいたために処理がおかしくなっていた。
+とくに、画像の行列による変形がおかしくなっていた。
+
+逆行列にして処理するとうまくいった。
+その後、理解しやすくするため変数名を変更した。さらに理解が深まった。
+'''
+
 import numpy as np
 import cv2
-from matplotlib import pyplot as plt
 
 MIN_MATCH_COUNT = 10
+RATIO = 0.5
 
 # 画像1
-img2 = cv2.imread("img1.png")
+orig = cv2.imread("orig.png")
 # 画像2
-img1 = cv2.imread("img2.png")
+collage = cv2.imread("collage.png")
 
 # A-KAZE検出器の生成
 akaze = cv2.AKAZE_create()
 
 # 特徴量の検出と特徴量ベクトルの計算
-kp1, des1 = akaze.detectAndCompute(img1, None)
-kp2, des2 = akaze.detectAndCompute(img2, None)
-
+kp_orig, des_orig = akaze.detectAndCompute(orig, None)
+kp_collage, des_collage = akaze.detectAndCompute(collage, None)
 
 # Brute-Force Matcher生成
 bf = cv2.BFMatcher()
 
 # 特徴量ベクトル同士をBrute-Force&KNNでマッチング
-matches = bf.knnMatch(des1, des2, k=2)
-
+matches = bf.knnMatch(des_orig, des_collage, k=2)
 
 # store all the good matches as per Lowe's ratio test.
-good = []
-for m,n in matches:
-	if m.distance < 0.7*n.distance:
-		good.append(m)
+	good = []
+	for m,n in matches:
+		if m.distance < RATIO*n.distance:
+			good.append(m)
 
-if len(good)>MIN_MATCH_COUNT:
-	src_pts = np.float32([ kp1[m.queryIdx].pt for m in good ]).reshape(-1,1,2)
-	dst_pts = np.float32([ kp2[m.trainIdx].pt for m in good ]).reshape(-1,1,2)
+	if len(good)>=MIN_MATCH_COUNT:
+		orig_pts = np.float32([ kp_orig[m.queryIdx].pt for m in good ]).reshape(-1,1,2)
+		collage_pts = np.float32([ kp_collage[m.trainIdx].pt for m in good ]).reshape(-1,1,2)
 
-	M, mask = cv2.findHomography(src_pts, dst_pts, cv2.RANSAC,5.0)
-	matchesMask = mask.ravel().tolist()
+		M, mask = cv2.findHomography(orig_pts, collage_pts, cv2.RANSAC,5.0)
 	
-	h,w = img1.shape[:2]
-	#h = img1.shape[0]
-	#w = img1.shape[1]
-	pts = np.float32([ [0,0],[0,h-1],[w-1,h-1],[w-1,0] ]).reshape(-1,1,2)
-	dst = cv2.perspectiveTransform(pts,M)
+		h,w = orig.shape[:2]
 
-	img2 = cv2.polylines(img2,[np.int32(dst)],True,255,3, cv2.LINE_AA)
-	cv2.imwrite('img3a.png', img2)
-else:
-	print ('hoge')
-	matchesMask = None
+		converted_collage = cv2.warpPerspective(collage, np.linalg.inv(M), (w, h) )
 
-draw_params = dict(matchColor = (0,255,0), # draw matches in green color
-	singlePointColor = None,
-	matchesMask = matchesMask, # draw only inliers
-	flags = 2)
-
-img3 = cv2.drawMatches   (img1, kp1, img2, kp2, good, None, **draw_params)
-#img3 = cv2.drawMatchesKnn(img1, kp1, img2, kp2, good, None, flags=2)
-
-cv2.imwrite('img3.png', img3)
+		cv2.imwrite('converted_collage.png', converted_collage)
+	
+	else:
+		print("not enough matching key point")

4c597f13324bf71b6bd54fed3a23a6aa66360f20
NaokiSato102 <s17c1066nq@s.chibakoudai.jp>
2019-10-03 01:39:52 +0900

    一旦コミット

--- collage_diff_detector.py
+++ collage_diff_detector.py
@@ -1,15 +1,27 @@
+'''
+https://tat-pytone.hatenablog.com/entry/2019/03/20/195949
+'''
+
 import cv2 #OpenCVのインポート
 import numpy as np #numpyをnpという名前でインポート
 
-img1 = cv2.imread('castle1.png',0) #画像castle1.pngをグレースケールで読み込む
-img2 = cv2.imread('castle2.png',0) #画像castle2.pngをグレースケールで読み込む
+
+orig = cv2.imread("orig.png",0)
+collage_conv = cv2.imread("collage_converted.png",0)
+
+
 
 #img1,img2の各要素を比較し、等しければ0、異なれば1の値を持つ配列comparisonを生成
-comparison=np.where(img1 == img2, 0, 1)
+comparison=np.where(orig == collage_conv, 0, 1)
+print(type(comparison))
+
+print(type(collage_conv))
 #img1とcomparisonの要素の積、配列differenceを生成
 #2つの画像の要素が等しい部分は0が掛けられるため0(黒)となり
 #2つの画像の要素が等しい部分は1が掛けられるため値を持つ
 #グレースケール画像として出力するためdtype = np.uint8を指定する
-difference=np.array(img1*comparison,dtype = np.uint8)
+#comparison = collage_conv*comparison
+comparison = np.dot(collage_conv,comparison)
+difference=np.array( comparison ,dtype = np.uint8)
 
-cv2.imwrite('difference.png',difference) #ファイル名difference.pngでdifferenceを保存
+cv2.imwrite('collage_difference.png',difference) #ファイル名difference.pngでdifferenceを保存
